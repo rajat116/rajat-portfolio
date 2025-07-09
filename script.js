7263044351599196a99fa51d606cbc53521290f3
rajat116 <rajatgupta116@gmail.com>
2025-07-09 10:10:52 +0000

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const roles = ["Data Scientist", "ML Engineer", "AI Researcher"];
 let i = 0;
-
 setInterval(() => {
   document.getElementById("animated-text").textContent = roles[i];
   i = (i + 1) % roles.length;
